1a99cbf64a7f9015306dbc22f34ad291315a5521
Andre Nagakara <andre.nagakara@gmail.com>
2021-11-01 20:09:51 -0300

    Inicio do projeto

---
+++ BlueAcademy_CaixaEletronico/src/main/java/com/BlueAcademy_CaixaEletronico/demo/BlueAcademyCaixaEletronicoApplication.java
@@ -0,0 +1,13 @@
+package com.BlueAcademy_CaixaEletronico.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueAcademyCaixaEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueAcademyCaixaEletronicoApplication.class, args);
+	}
+
+}
---
+++ BlueAcademy_CaixaEletronico/src/test/java/com/BlueAcademy_CaixaEletronico/demo/BlueAcademyCaixaEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlueAcademy_CaixaEletronico.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueAcademyCaixaEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
